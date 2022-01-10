6c66f15b4986fa618c669f54df851ced5c43d8ed
VinayKumarParas <vinay.parastechnologies.in>
2022-01-10 23:16:33 +0530

    commited

--- ToasterLibrary/src/main/java/com/example/toasterlibrary/ToasterMessage.kt
+++ ToasterLibrary/src/main/java/com/example/toasterlibrary/ToasterMessage.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.widget.Toast
 
 class ToasterMessage {
+
     companion object{
         fun showToastMessage(context: Context, message:String){
             Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
